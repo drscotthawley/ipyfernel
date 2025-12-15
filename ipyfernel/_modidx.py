9d344afb64ca96aa5b978d0d037e4ef610423ab7
Solveit <nobody@fast.ai>
2025-12-15 02:47:24 +0000

    eval:false for docs build

--- ipyfernel/_modidx.py
+++ ipyfernel/_modidx.py
@@ -5,6 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://drscotthawley.github.io',
                 'git_url': 'https://github.com/drscotthawley/ipyfernel',
                 'lib_path': 'ipyfernel'},
-  'syms': { 'ipyfernel.core': { 'ipyfernel.core.foo': ('core.html#foo', 'ipyfernel/core.py'),
-                                'ipyfernel.core.local': ('core.html#local', 'ipyfernel/core.py'),
-                                'ipyfernel.core.set_ssh_config': ('core.html#set_ssh_config', 'ipyfernel/core.py')}}}
+  'syms': {'ipyfernel.core': {'ipyfernel.core.set_ssh_config': ('core.html#set_ssh_config', 'ipyfernel/core.py')}}}
